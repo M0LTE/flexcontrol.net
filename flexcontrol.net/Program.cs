1f34655b591288e94ce654671e241c0011b6c869
Tom <tj@tjej.uk>
2018-04-13 20:41:01 +0100

    Initial commit

---
+++ flexcontrol.net/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace flexcontrol.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fn = new FlexKnob();
+            fn.OnButtonPush += ButtonPushHandler;
+            fn.OnRotate += RotateHandler;
+            fn.Run();
+        }
+
+        static void ButtonPushHandler(Button which, TapType how)
+        {
+            Console.WriteLine($"{which} {how}");
+        }
+
+        static void RotateHandler(Direction dir, int speed)
+        {
+            Console.WriteLine($"{dir} {speed}");
+        }
+    }
+}
